12029dac62a6e651527e43c8066a8c3001eedb5a
plusls <plusls@qq.com>
2020-08-29 02:14:57 +0800

    格式化代码

--- src/main/java/cn/apisium/nekoguard/fabric/mixin/vehicle/damage/MixinBoatEntity.java
+++ src/main/java/cn/apisium/nekoguard/fabric/mixin/vehicle/damage/MixinBoatEntity.java
@@ -8,10 +8,8 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.vehicle.BoatEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BoatEntity.class)
